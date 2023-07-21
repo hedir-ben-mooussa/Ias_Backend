393b5600c3a6ef353b9637df72bb5b90c63ecbda
unknown <hedirbenmoussa71@gmail.com>
2023-07-21 14:30:29 +0200

    first commit

---
+++ src/main/java/tn/ias/IasWebsite/IasWebsiteApplication.java
@@ -0,0 +1,13 @@
+package tn.ias.IasWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IasWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IasWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tn/ias/IasWebsite/IasWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.ias.IasWebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IasWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
